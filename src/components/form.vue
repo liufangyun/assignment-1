6a0579637032318caef056c575839bef7880fae6
Fangyun,Liu <liu00433@algonquinlive.com>
2019-02-08 14:36:51 -0500

    finish the assignment-1

---
+++ src/components/form.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-form ref="form" :model="form" label-width="120px">
+  <el-form-item label="First name">
+    <el-input v-model="form.name"></el-input>
+  </el-form-item>
+   <el-form-item label="Last name">
+    <el-input v-model="form.name"></el-input>
+  </el-form-item>
+    <el-form-item label="Email">
+    <el-input v-model="form.name"></el-input>
+  </el-form-item>
+    <el-form-item label="Claim Request Content">
+    <el-input type="textarea" v-model="form.desc"></el-input>
+  </el-form-item>
+   <el-form-item label=" ">
+    <el-checkbox-group v-model="form.type">
+      <el-checkbox label="I agree" name="type"></el-checkbox>
+    </el-checkbox-group>
+  </el-form-item>
+  <el-popover
+    placement="bottom"
+    title="Title"
+    width="200"
+    trigger="click"
+    content="this is content, this is content, this is content">
+    <el-button slot="reference">Submit</el-button>
+  </el-popover>
+  
+</el-form>
+
+</template>
+
+<script src="./form.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./form.css" scoped></style>
